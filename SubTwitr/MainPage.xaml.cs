00a63262e243f504a17a6ce31c2270ec8cec98e2
= <kenny.lowe@outlook.com>
2016-07-20 12:28:55 +0100

    Initial commit

--- SubTwitr/MainPage.xaml.cs
+++ SubTwitr/MainPage.xaml.cs
@@ -12,19 +12,132 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Microsoft.WindowsAzure.Storage; 
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.File;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using System.Windows;
 
 namespace SubTwitr
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+    
     public sealed partial class MainPage : Page
     {
+        public StorageFile file;
+        public FileInfo fileInfo;
         public MainPage()
         {
+
             this.InitializeComponent();
+        }
+
+        private async void FilePicker_Click(object sender, RoutedEventArgs e)
+        {
+                FileOpenPicker openPicker = new FileOpenPicker();
+                openPicker.ViewMode = PickerViewMode.Thumbnail;
+                openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+                openPicker.FileTypeFilter.Add(".mp4");
+                openPicker.FileTypeFilter.Add(".avi");
+                openPicker.FileTypeFilter.Add(".mpg");
+                openPicker.FileTypeFilter.Add(".mpeg");
+
+            PickFile.BorderBrush= new Windows.UI.Xaml.Media.SolidColorBrush(Windows.UI.Color.FromArgb(0, 0, 0, 0));
+            
+
+
+            file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Application now has read/write access to the picked file
+                OutputTextBlock.Text = "File Selected: " + file.Name;
+                var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                mediaElement.SetSource(stream, file.ContentType);
+                playPause.Text = "Tap to Play";
+                playPauseBorder.Visibility = Visibility.Visible;
+                playPause.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                OutputTextBlock.Text = "Operation cancelled.";
+            } 
+        }
+
+
+        private void SendFile_Click(object sender, RoutedEventArgs e)
+        {
+            if (file != null)
+            {
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=subtwitr;AccountKey=y2FHEejelfEDF8w38kdYWSdpgBcJ39DsHU2fkjFbJT80fUPS8CYvg73QIUpO5pJYQZs8QImkQdN2s91cWvawzA==");
+                CloudFileClient fileClient = storageAccount.CreateCloudFileClient();
+                CloudFileShare share = fileClient.GetShareReference("subtwitr");
+                CloudFileDirectory dir = share.GetRootDirectoryReference();
+                CloudFile destFile = dir.GetFileReference(file.Name);
+                destFile.UploadFromFileAsync(file);
+            }
+        }
+
+        private void TextBlock_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+        { 
+        }
+
+        private void mediaElement_PointerCanceled(object sender, PointerRoutedEventArgs e)
+        {
+        }
+
+
+        private void playPause_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if ((mediaElement.CurrentState.ToString() == "Stopped") || (mediaElement.CurrentState.ToString() == "Paused"))
+            {
+                playPauseBorder.Visibility = Visibility.Collapsed;
+                playPause.Visibility = Visibility.Collapsed;
+                mediaElement.Play();
+            }
+        }
+
+        private void playPauseBorder_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if ((mediaElement.CurrentState.ToString() == "Stopped") || (mediaElement.CurrentState.ToString() == "Paused"))
+            {
+                playPauseBorder.Visibility = Visibility.Collapsed;
+                playPause.Visibility = Visibility.Collapsed;
+                mediaElement.Play();
+            }
+        }
+
+        private void PickFile_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+
+        }
+
+        private void mediaElement_PointerCanceled_1(object sender, PointerRoutedEventArgs e)
+        {
+            if (mediaElement.CurrentState.ToString() == "Playing")
+            {
+                mediaElement.Pause();
+            }
+            if ((mediaElement.CurrentState.ToString() == "Stopped") || (mediaElement.CurrentState.ToString() == "Paused"))
+            {
+                mediaElement.Play();
+                playPauseBorder.Visibility = Visibility.Collapsed;
+                playPause.Visibility = Visibility.Collapsed;
+            }
+
+        }
+
+        private void mediaElement_Tapped_1(object sender, TappedRoutedEventArgs e)
+        {
+            if (mediaElement.CurrentState.ToString() == "Playing")
+            {
+                mediaElement.Pause();
+            }
+            if ((mediaElement.CurrentState.ToString() == "Stopped") || (mediaElement.CurrentState.ToString() == "Paused"))
+            {
+                mediaElement.Play();
+                playPauseBorder.Visibility = Visibility.Collapsed;
+                playPause.Visibility = Visibility.Collapsed;
+            }
         }
     }
 }
