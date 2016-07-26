b4e5e7a765204ad8f252d40ce4a23feaa2f5d22b
= <kenny.lowe@outlook.com>
2016-07-26 16:34:01 +0100

    Really for sure added Settings...

---
+++ SubTwitr/Options.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace SubTwitr
+{ 
+    public sealed partial class Options : Page
+    {
+        public Options()
+        {
+            this.InitializeComponent();
+        }
+
+        private void backButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage), null);
+        }
+    }
+}
+
